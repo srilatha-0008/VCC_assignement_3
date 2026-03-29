ffa8990f3aaab54dfadc7f8e71f09909424a3866
srilatha-jb <136793378+srilatha-0008@users.noreply.github.com>
2026-03-29 16:09:24 +0530

    Create install_node_exporter.sh

---
+++ scripts/install_node_exporter.sh
@@ -0,0 +1,5 @@
+cd /tmp
+wget https://github.com/prometheus/node_exporter/releases/download/v1.8.2/node_exporter-1.8.2.linux-amd64.tar.gz
+tar -xvf node_exporter-1.8.2.linux-amd64.tar.gz
+cd node_exporter-1.8.2.linux-amd64
+./node_exporter
